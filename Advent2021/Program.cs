450eaaed350534a599830d480df79cb7a1ec5361
Andrew Theken <andrew@attr.io>
2022-11-01 08:20:31 -0400

    Start aoc2021

---
+++ Advent2021/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+
+public class Program
+{
+    private static string FindInput(string inputName, string? path = null)
+    {
+        path ??= Assembly.GetExecutingAssembly().Location;
+        var current = Directory.GetParent(path);
+        var filename = $"{current?.FullName}/input/{inputName}";
+        if (current != null)
+        {
+            if (File.Exists(filename))
+            {
+                return filename;
+            }
+            else
+            {
+                return FindInput(inputName, current.FullName);
+            }
+        }
+        else
+        {
+            throw new FileNotFoundException();
+        }
+    }
+    
+    public static async Task Main()
+    {
+        var input = File.ReadLines(FindInput("puzzle1.txt"));
+        int? previous = null;
+        var increasingCount = 0;
+        
+        foreach (var i in input.Select(k => int.Parse(k)))
+        {
+            if (previous.HasValue && i > previous)
+            {
+                increasingCount++;
+            }
+            previous = i;
+        }
+        Console.WriteLine(increasingCount);
+    }
+}

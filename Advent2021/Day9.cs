8e89ca9cd35d4e5b3089646f24fa9552746dee77
Andrew Theken <andrew@attr.io>
2022-11-02 07:36:46 -0400

    General code cleanup.

--- Advent2021/Day9.cs
+++ Advent2021/Day9.cs
@@ -12,45 +12,34 @@ public class Day9
         _output = outputHelper;
     }
 
-    protected record Point(int row, int column, int value);
+    [Fact]
+    public void Puzzle1()
+    {
+        var grid = new Grid("day9.txt".ReadInputLines());
+
+        _output.WriteLine(grid.FindLowPoints()
+            .Sum(k => k.Value + 1).ToString());
+    }
+
+    [Fact]
+    public void Puzzle2()
+    {
+        var grid = new Grid("day9.txt".ReadInputLines());
+        var largestBasins = grid.GetBasins().OrderByDescending(k => k.Size).Take(3);
+        var size = largestBasins.Aggregate(1, (seed, current) => seed * current.Size);
+        _output.WriteLine(size.ToString());
+    }
 
     private class Grid
     {
-        public class Basin
-        {
-            private HashSet<Point> _points = new ();
-
-            public bool AddPoint(Point pt)
-            {
-                if (pt.value < 9)
-                {
-                    return _points.Add(pt);
-                }
-                else
-                {
-                    return false;
-                }
-            }
-
-            /// <summary>
-            /// the basin is only valid if there is exactly one point at the minimum.
-            /// </summary>
-            public bool IsValidBasin => _points.Count(f => f.value == _points.Min(k => k.value)) == 1;
-
-            /// <summary>
-            /// The size of this basin.
-            /// </summary>
-            public int Size => _points.Count;
-        }
-        
-        private int[][] Points { get; }
-        
         public Grid(IEnumerable<string> lines)
         {
             Points = lines.Select(l => l.Select(f => int.Parse(f.ToString())).ToArray()).ToArray();
         }
 
-        public Basin ExpandBasin(Point searchPoint, Basin? existingBasin = null)
+        private int[][] Points { get; }
+
+        private Basin ExpandBasin(Point searchPoint, Basin? existingBasin = null)
         {
             existingBasin ??= new Basin();
 
@@ -61,36 +50,37 @@ public class Day9
                     existingBasin = ExpandBasin(pt, existingBasin);
                 }
             }
-            
+
             return existingBasin;
         }
 
         private IEnumerable<Point> GetNeighbors(Point point)
         {
-            if (point.row - 1 >= 0)
+            if (point.Row - 1 >= 0)
             {
-                yield return new Point(point.row - 1, point.column, Points[point.row - 1][point.column]);
+                yield return new Point(point.Row - 1, point.Column, Points[point.Row - 1][point.Column]);
             }
 
-            if (point.row + 1 <= Points.Length - 1)
+            if (point.Row + 1 <= Points.Length - 1)
             {
-                yield return new Point(point.row + 1, point.column, Points[point.row + 1][point.column]);
+                yield return new Point(point.Row + 1, point.Column, Points[point.Row + 1][point.Column]);
             }
 
-            if (point.column - 1 >= 0)
+            if (point.Column - 1 >= 0)
             {
-                yield return new Point(point.row, point.column - 1, Points[point.row][point.column - 1]);
+                yield return new Point(point.Row, point.Column - 1, Points[point.Row][point.Column - 1]);
             }
 
-            if (point.column + 1 <= Points[point.row].Length - 1)
+            if (point.Column + 1 <= Points[point.Row].Length - 1)
             {
-                yield return new Point(point.row, point.column + 1, Points[point.row][point.column + 1]);
+                yield return new Point(point.Row, point.Column + 1, Points[point.Row][point.Column + 1]);
             }
         }
 
         public IEnumerable<Basin> GetBasins() =>
-            FindLowPoints().Select(k => ExpandBasin(k)).Where(k => k.IsValidBasin);
-        
+            FindLowPoints().Select(k => ExpandBasin(k))
+                .Where(k => k.IsValidBasin);
+
         public IEnumerable<Point> FindLowPoints()
         {
             foreach (var (row, rowValues) in Points.Each())
@@ -102,30 +92,41 @@ public class Day9
                     // if the value before this one, and after this one in this line,
                     // and the value above and below this one, are all > val, this point
                     // is the lowest.
-                    if (GetNeighbors(subject).All(k => k.value > subject.value))
+                    if (GetNeighbors(subject).All(k => k.Value > subject.Value))
                     {
                         yield return subject;
                     }
                 }
             }
         }
-    }
-    
-    [Fact]
-    public void Puzzle1()
-    {
-        var grid = new Grid("day9.txt".ReadInputLines());
-        
-        _output.WriteLine(grid.FindLowPoints()
-            .Sum(k=> k.value + 1).ToString());
-    }
 
-    [Fact]
-    public void Puzzle2()
-    {
-        var grid = new Grid("day9.txt".ReadInputLines());
-        var largestBasins = grid.GetBasins().OrderByDescending(k => k.Size).Take(3);
-        var size = largestBasins.Aggregate(1, (seed, current) => seed * current.Size);
-        _output.WriteLine(size.ToString());
+        public record Point(int Row, int Column, int Value);
+
+        public class Basin
+        {
+            private readonly HashSet<Point> _points = new();
+
+            /// <summary>
+            /// the basin is only valid if there is exactly one point at the minimum.
+            /// </summary>
+            public bool IsValidBasin => _points.Count(f => f.Value == _points.Min(k => k.Value)) == 1;
+
+            /// <summary>
+            /// The size of this basin.
+            /// </summary>
+            public int Size => _points.Count;
+
+            public bool AddPoint(Point pt)
+            {
+                if (pt.Value < 9)
+                {
+                    return _points.Add(pt);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
     }
 }

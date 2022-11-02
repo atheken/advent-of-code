1da4e5004c657ea439bf1d4dfbeb2409f23a57cf
Andrew Theken <andrew@attr.io>
2022-11-02 08:49:12 -0400

    Add initial state for day 10.

---
+++ Advent2021/Day10.cs
@@ -0,0 +1,26 @@
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Advent2021;
+
+public class Day10
+{
+    private readonly ITestOutputHelper _output;
+
+    public Day10(ITestOutputHelper outputHelper)
+    {
+        _output = outputHelper;
+    }
+
+    [Fact]
+    public void Puzzle1()
+    {
+    
+    }
+
+    [Fact]
+    public void Puzzle2()
+    {
+        throw new NotImplementedException();
+    }
+}

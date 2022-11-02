e318eb18b95a3d1896740af1a2198201cd0dd021
Andrew Theken <andrew@attr.io>
2022-11-02 07:32:51 -0400

    Complete day 9.

--- Advent2021/Day9.cs
+++ Advent2021/Day9.cs
@@ -12,74 +12,120 @@ public class Day9
         _output = outputHelper;
     }
 
-    private class Level
+    protected record Point(int row, int column, int value);
+
+    private class Grid
     {
-        private int[] Points { get; }
-        
-        public Level(string line, Level? previousLevel)
+        public class Basin
         {
-            Points = line.Select(f => int.Parse(f.ToString())).ToArray();
-            if (previousLevel != null)
-            {
-                previousLevel.NextLevel = this;
-            }
-            PreviousLevel = previousLevel;
+            private HashSet<Point> _points = new ();
 
-        }
-
-        private int this[int index]
-        {
-            get
+            public bool AddPoint(Point pt)
             {
-                if (index < 0 || index > Points.Length - 1)
+                if (pt.value < 9)
                 {
-                    return 9;
+                    return _points.Add(pt);
                 }
                 else
                 {
-                    return Points[index];
+                    return false;
                 }
             }
+
+            /// <summary>
+            /// the basin is only valid if there is exactly one point at the minimum.
+            /// </summary>
+            public bool IsValidBasin => _points.Count(f => f.value == _points.Min(k => k.value)) == 1;
+
+            /// <summary>
+            /// The size of this basin.
+            /// </summary>
+            public int Size => _points.Count;
         }
-
-        public int LevelCount => (PreviousLevel?.LevelCount ?? 0) + 1;
-
-        public IEnumerable<int> FindLowPoints()
+        
+        private int[][] Points { get; }
+        
+        public Grid(IEnumerable<string> lines)
         {
-            var retval = new List<int>();
-            foreach (var (i, val) in Points.Each())
-            {
-                // if the value before this one, and after this one in this line,
-                // and the value above and below this one, are all > val, this point
-                // is the lowest.
-                if (this[i - 1] > val && 
-                    this[i + 1] > val && 
-                    (PreviousLevel?[i] ?? 9) > val &&
-                    (NextLevel?[i] ?? 9) > val)
-                {
-                    retval.Add(val + 1);
-                }
-            }
-
-            return retval.Concat(PreviousLevel?.FindLowPoints() ?? new int[0]);
-
+            Points = lines.Select(l => l.Select(f => int.Parse(f.ToString())).ToArray()).ToArray();
         }
 
-        private Level? PreviousLevel { get; }
-        private Level? NextLevel { get; set; }
+        public Basin ExpandBasin(Point searchPoint, Basin? existingBasin = null)
+        {
+            existingBasin ??= new Basin();
+
+            if (existingBasin.AddPoint(searchPoint))
+            {
+                foreach (var pt in GetNeighbors(searchPoint))
+                {
+                    existingBasin = ExpandBasin(pt, existingBasin);
+                }
+            }
+            
+            return existingBasin;
+        }
+
+        private IEnumerable<Point> GetNeighbors(Point point)
+        {
+            if (point.row - 1 >= 0)
+            {
+                yield return new Point(point.row - 1, point.column, Points[point.row - 1][point.column]);
+            }
+
+            if (point.row + 1 <= Points.Length - 1)
+            {
+                yield return new Point(point.row + 1, point.column, Points[point.row + 1][point.column]);
+            }
+
+            if (point.column - 1 >= 0)
+            {
+                yield return new Point(point.row, point.column - 1, Points[point.row][point.column - 1]);
+            }
+
+            if (point.column + 1 <= Points[point.row].Length - 1)
+            {
+                yield return new Point(point.row, point.column + 1, Points[point.row][point.column + 1]);
+            }
+        }
+
+        public IEnumerable<Basin> GetBasins() =>
+            FindLowPoints().Select(k => ExpandBasin(k)).Where(k => k.IsValidBasin);
+        
+        public IEnumerable<Point> FindLowPoints()
+        {
+            foreach (var (row, rowValues) in Points.Each())
+            {
+                foreach (var (column, val) in rowValues.Each())
+                {
+                    var subject = new Point(row, column, val);
+
+                    // if the value before this one, and after this one in this line,
+                    // and the value above and below this one, are all > val, this point
+                    // is the lowest.
+                    if (GetNeighbors(subject).All(k => k.value > subject.value))
+                    {
+                        yield return subject;
+                    }
+                }
+            }
+        }
     }
     
     [Fact]
     public void Puzzle1()
     {
-        Level? currentLevel = null;
-        foreach (var line in "day9.txt".ReadInputLines())
-        {
-            var parsedLevel = new Level(line, currentLevel);
-            currentLevel = parsedLevel;
-        }
+        var grid = new Grid("day9.txt".ReadInputLines());
+        
+        _output.WriteLine(grid.FindLowPoints()
+            .Sum(k=> k.value + 1).ToString());
+    }
 
-        var levelCount = currentLevel.LevelCount;
-        _output.WriteLine(currentLevel!.FindLowPoints().Sum().ToString());
+    [Fact]
+    public void Puzzle2()
+    {
+        var grid = new Grid("day9.txt".ReadInputLines());
+        var largestBasins = grid.GetBasins().OrderByDescending(k => k.Size).Take(3);
+        var size = largestBasins.Aggregate(1, (seed, current) => seed * current.Size);
+        _output.WriteLine(size.ToString());
     }
 }
